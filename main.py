c54743ce54342f8d103abad80b16e7d490f0d96a
Syldup <syd9up@gmail.com>
2020-10-07 10:51:14 +0200

    add test page dans fix bug

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from math import sin, cos, atan2, radians
 import asyncio
 import websockets
+import requests
 import json
 
 
@@ -20,12 +21,34 @@ def distance_earth_coo(lat1, lon1, lat2, lon2) -> float:
     return earthRadiusKm * c
 
 
-async def echo(websocket: websockets.WebSocketServerProtocol, path: str):
+async def recv(websocket: websockets.WebSocketServerProtocol, path: str):
     async for message in websocket:
-        dist = distance_earth_coo(**json.loads(message))
-        await websocket.send(dist)
+        print("recv", message)
+        coo_data = json.loads(message)
+        coo = {
+            'lon1': coo_data['X'],
+            'lat1': coo_data['Y'],
+        }
+        # resp = list(requests.get('http://api.github.com/position').json)
+        resp = [{'latitude': 1.0, 'longitude': 1.0, 'id': 0}]
+
+        data = [{
+            'name': food['id'],
+            'distance': distance_earth_coo(lon2=food['longitude'], lat2=food['latitude'], **coo),
+            'coordinates': {
+                'X': food['longitude'],
+                'Y': food['latitude'],
+            }
+        } for food in resp]
+
+        data.sort(key=lambda x: x['distance'])
+
+        await websocket.send(json.dumps({
+            'coo': coo_data,
+            'data': data[:10]
+        }))
 
 
-asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, 'localhost', 8765))
+asyncio.get_event_loop().run_until_complete(websockets.serve(recv, 'localhost', 5050))
+print("Start on localhost:5050")
 asyncio.get_event_loop().run_forever()
